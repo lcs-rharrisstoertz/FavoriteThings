306eafd5437bbf754725d9e1b819456c91ff8140
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-04 09:47:52 -0500

    Initial Commit

---
+++ FavoriteThings/FavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsApp.swift
+//  FavoriteThings
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-04.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
